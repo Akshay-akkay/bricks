9f52fe441983d31e9ee950952756cf1b7a8a6312
akshayApplore <akshay.sharma@applore.in>
2022-11-02 10:22:27 +0530

    Added auto_route import

--- feature_auto_route_bloc/__brick__/{{feature_name}}/view/{{feature_name}}_view.dart
+++ feature_auto_route_bloc/__brick__/{{feature_name}}/view/{{feature_name}}_view.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:{{package_name}}/{{{path}}}/{{feature_name}}.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
